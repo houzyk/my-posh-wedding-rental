08582346c4b8d68167728ad8c4bc226860302b2d
Christian Bongard <christian.bongard90@gmail.com>
2021-11-08 16:28:52 +0400

    added user reference to items

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, :phone_number, :address, presence: true
+  has_many :items
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_11_08_114659) do
+ActiveRecord::Schema.define(version: 2021_11_08_122707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_11_08_114659) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_11_08_114659) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
