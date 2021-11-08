08b302a6a4c33ee1b4e3e6218e5e9f6362a70947
Christian Bongard <christian.bongard90@gmail.com>
2021-11-08 15:50:30 +0400

    added validation to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :price, presence: true, numericality: true
 end
