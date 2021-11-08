cf90ced51c21cfbdf40b01d69e94cb0184f0afd7
Christian Bongard <christian.bongard90@gmail.com>
2021-11-08 16:00:58 +0400

    refactored validation for description and category

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :description, presence: true
-  validates :category, presence: true
+  validates :description, :category, presence: true
   validates :price, presence: true, numericality: true
 end
