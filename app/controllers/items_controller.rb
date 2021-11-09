0de6fdd3dd500e240f0737f59b24199159986ac9
houzairkoussa <houzairmk@icloud.com>
2021-11-09 11:53:02 +0400

    ..

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,36 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: %i[show edit update]
   after_action :authorize_item
   def index
     @items = Item.all
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def create
+  end
+
+  def edit
+  end
+
+  def update
+    @item.update(items_params)
+    redirect_to item_path(@item)
   end
 
   # def search
   # end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def items_params
+    params.require(:item).permit(:name, :description, :price, :category)
+  end
 
   def authorize_item
     authorize @item
