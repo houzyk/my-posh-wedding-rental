428ed5582395a85bdde568f17e69d82b99f6302f
Christian Bongard <christian.bongard90@gmail.com>
2021-11-09 12:50:54 +0400

    removed pundit on index and show

--- app/policies/item_policy.rb
+++ app/policies/item_policy.rb
@@ -8,4 +8,16 @@ class ItemPolicy < ApplicationPolicy
   def create?
     true
   end
+
+  def update?
+    record.user == user
+  end
+
+  def show?
+    true
+  end
+
+  def index?
+    true
+  end
 end
