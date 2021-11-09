09569fc14fd0ce54a53eaee2da066c77df30e699
Christian Bongard <christian.bongard90@gmail.com>
2021-11-09 17:42:14 +0400

    added booking new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,53 +1,66 @@
 class BookingsController < ApplicationController
-  before_action: set_booking, only[:show, :destroy]
+  # before_action :set_booking, only: %i[show destroy]
 
   def new
-    @item = Item.find(params[item_id])
+    @item = Item.find(params[:item_id])
     @booking = Booking.new
+    authorize @booking
   end
 
   def create
-    @item = Item.find(params[item_id])
+    @item = Item.find(params[:item_id])
     @booking = Booking.new(booking_params)
     @booking.item = @item
-    @booking.save
-    redirect_to item_path(@item)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+    authorize @booking
   end
+  # def create
+  #   @item = Item.find(params[item_id])
+  #   @booking = Booking.new(booking_params)
+  #   @booking.item = @item
+  #   @booking.save
+  #   redirect_to item_path(@item)
+  # end
 
-  def edit
-    @item = Item.find(params[item_id])
-    @booking = Booking.find(params[:id])
-  end
+  # def edit
+  #   @item = Item.find(params[item_id])
+  #   @booking = Booking.find(params[:id])
+  # end
 
-  def update
-    @booking = Booking.find(params[:id])
-    @booking.update(params[:booking])
-  end
+  # def update
+  #   @booking = Booking.find(params[:id])
+  #   @booking.update(params[:booking])
+  # end
 
-  
 
-  def index
-    @bookings = Booking.all
-  end
 
-  def show
-  end
+  # def index
+  #   @bookings = Booking.all
+  # end
 
-  def destroy
-    @booking.destroy
+  # def show
+  # end
 
-    redirect_to bookings_path
-  end
+  # def destroy
+  #   @booking.destroy
+
+  #   redirect_to bookings_path
+  # end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:item_id, :user_id)
+    params.require(:booking).permit(:item_id, :user_id, :comment)
   end
 
-  def set_booking
-    @booking = Booking.find(params[:id])
-  end
+  # def set_booking
+  #   @booking = Booking.find(params[:id])
+  # end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
     collection do
       get :search
     end
-    member do
       resources :bookings
-    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
