755a8fcbae881fc0255091b1738a8fea0806e207
dhanishtha goordoyal <dhanisthag@gmail.com>
2021-11-09 15:14:39 +0400

    bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,16 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+  end
+  
 end
