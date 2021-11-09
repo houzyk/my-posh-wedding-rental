e8527bc793cc9f19cf7c4807fc1d440e5065adbc
houzairkoussa <houzairmk@icloud.com>
2021-11-09 08:58:35 +0400

    corrected html for footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name phone_number address])
 
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name phone_number address]
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name phone_number address])
   end
 end
