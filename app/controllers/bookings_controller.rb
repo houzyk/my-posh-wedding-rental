ba81c968a2e53a7c81aa59fb086bdca0cb34cb0e
Yogaisan Ramasawmy <yogaisan@gmail.com>
2021-11-09 15:55:28 +0400

    created methods new, create, edit and update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,30 @@
 class BookingsController < ApplicationController
+  def new
+    @item = Item.find(params[item_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @item = Item.find(params[item_id])
+    @booking = Booking.new(booking_params)
+    @booking.item = @item
+    @booking.save
+    redirect_to item_path(@item)
+  end
+
+  def edit
+    @item = Item.find(params[item_id])
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(params[:booking])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:item_id, :renter_id)
+  end
 end
