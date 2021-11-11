ff253579d4fd618c6b89ac6efa95ffdee49b547b
Christian Bongard <christian.bongard90@gmail.com>
2021-11-11 13:01:50 +0400

    added category seed

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
 import "select2";
-// const category = ['Suits' ,'Wedding Dress', 'Bridesmaid', 'Indian Traditional', 'Muslim Traditional', 'Groomsman', 'Jewellery', 'Veils' ];
+
 const initSelect2 = () => {
   $(".select2").select2({
     placeholder: "Select a category",
